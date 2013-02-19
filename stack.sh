cc191364d8c8bbdeeb10e00404fef0ce2a1bb60d
stev5232 <steven.wilson@rackspace.com>
2013-02-19 10:14:05 -0500

    Fixed EXTRA_OPTS[@] for loop

--- stack.sh
+++ stack.sh
@@ -1080,7 +1080,7 @@ if is_service_enabled nova; then
         iniset $NOVA_CONF baremetal tftp_root /tftpboot
 
         # Define extra baremetal nova conf flags by defining the array ``EXTRA_BAREMETAL_OPTS``.
-        for I in "${EXTRA_BAREMETAL_OPTS[@]}"; do
+        for I in ${EXTRA_BAREMETAL_OPTS[@]}; do
            # Attempt to convert flags to options
            iniset $NOVA_CONF baremetal ${I//=/ }
         done
